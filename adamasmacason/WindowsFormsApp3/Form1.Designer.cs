70051ec0af9e015461a50022e3133c0be3694228
Yusuf Kaymaz <146130623+ysfkmz7@users.noreply.github.com>
2025-01-13 13:55:53 +0300

    Add files via upload

---
+++ adamasmacason/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mesaj = new System.Windows.Forms.Label();
+            this.kalanhakform = new System.Windows.Forms.Label();
+            this.tahmintxt = new System.Windows.Forms.TextBox();
+            this.tahminbutonu = new System.Windows.Forms.Button();
+            this.maskelisehir = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.mesaj.AutoSize = true;
+            this.mesaj.Font = new System.Drawing.Font("Times New Roman", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mesaj.Location = new System.Drawing.Point(78, 395);
+            this.mesaj.Name = "lblMessage";
+            this.mesaj.Size = new System.Drawing.Size(0, 36);
+            this.mesaj.TabIndex = 0;
+            // 
+            // lblRemainingAttempts
+            // 
+            this.kalanhakform.AutoSize = true;
+            this.kalanhakform.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kalanhakform.Location = new System.Drawing.Point(74, 179);
+            this.kalanhakform.Name = "lblRemainingAttempts";
+            this.kalanhakform.Size = new System.Drawing.Size(178, 32);
+            this.kalanhakform.TabIndex = 1;
+            this.kalanhakform.Text = "Kalan Hak: 6";
+            // 
+            // txtGuess
+            // 
+            this.tahmintxt.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tahmintxt.Location = new System.Drawing.Point(80, 118);
+            this.tahmintxt.MaxLength = 1;
+            this.tahmintxt.Name = "txtGuess";
+            this.tahmintxt.Size = new System.Drawing.Size(100, 40);
+            this.tahmintxt.TabIndex = 2;
+            this.tahmintxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnGuess
+            // 
+            this.tahminbutonu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tahminbutonu.Location = new System.Drawing.Point(80, 255);
+            this.tahminbutonu.Name = "btnGuess";
+            this.tahminbutonu.Size = new System.Drawing.Size(177, 47);
+            this.tahminbutonu.TabIndex = 3;
+            this.tahminbutonu.Text = "Tahmin Et";
+            this.tahminbutonu.UseVisualStyleBackColor = true;
+            // 
+            // lblMaskedCity
+            // 
+            this.maskelisehir.AutoSize = true;
+            this.maskelisehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskelisehir.Location = new System.Drawing.Point(74, 47);
+            this.maskelisehir.Name = "lblMaskedCity";
+            this.maskelisehir.Size = new System.Drawing.Size(0, 32);
+            this.maskelisehir.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp3.Properties.Resources.hangman0;
+            this.pictureBox1.Location = new System.Drawing.Point(602, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 277);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(907, 515);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.maskelisehir);
+            this.Controls.Add(this.tahminbutonu);
+            this.Controls.Add(this.tahmintxt);
+            this.Controls.Add(this.kalanhakform);
+            this.Controls.Add(this.mesaj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mesaj;
+        private System.Windows.Forms.Label kalanhakform;
+        private System.Windows.Forms.TextBox tahmintxt;
+        private System.Windows.Forms.Button tahminbutonu;
+        private System.Windows.Forms.Label maskelisehir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ adamasmacason/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        // Şehir isimlerini içeren bir liste oluşturuyoruz.
+        private List<string> sehirler = new List<string>
+        {
+            "İstanbul", "Ankara", "İzmir", "Bursa", "Adana", "Gaziantep",
+            "Konya", "Antalya", "Kayseri", "Mersin", "Sakarya", "Trabzon",
+            "Aydın", "Kocaeli", "Edirne", "Malatya", "Manisa", "Hatay",
+            "Diyarbakır", "Elazığ", "Eskişehir", "Sivas", "Çorum", "Rize",
+            "Zonguldak", "Kastamonu", "Nevşehir", "Uşak", "Niğde", "Bolu",
+            "Ordu", "Tekirdağ", "Aksaray", "Karabük", "Düzce", "Batman",
+            "Şırnak", "Isparta", "Bartın", "Bayburt", "Bingöl", "Bitlis",
+            "Gümüşhane", "Kars", "Kırklareli", "Kırıkkale", "Osmaniye",
+            "Artvin", "Amasya", "Giresun", "Sinop", "Yozgat", "Kahramanmaraş",
+            "Tunceli", "Adıyaman", "Çankırı", "Karaman", "Mardin", "Muğla"
+        };
+
+        // Seçilen şehir, gizli olan şehir, kalan tahmin hakkı ve tahmin edilen harfler
+        private string secilensehir;
+        private string maskesehir;
+        private int kalanhak;
+        private HashSet<char> guessedLetters;
+
+        // Adam asmaca resimlerini tutacak dizi
+        private Image[] hangmanImages;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadHangmanImages();  // Adam asmaca resimlerini yükle
+            oyunbasla();        // Yeni bir oyun başlat
+
+            // Butonun rengini ve tıklama olayını ayarlıyoruz
+            tahminbutonu.BackColor = Color.BlueViolet;
+            tahminbutonu.Click += new EventHandler(btnGuess_Click);
+        }
+
+        // Adam asmaca resimlerini yükleme fonksiyonu
+        private void LoadHangmanImages()
+        {
+            try
+            {
+                // Resimlerin dosyalardan yüklenmesi
+                hangmanImages = new Image[]
+                {
+                    Properties.Resources.hangman0,
+                    Properties.Resources.hangman1,
+                    Properties.Resources.hangman2,
+                    Properties.Resources.hangman3,
+                    Properties.Resources.hangman4,
+                    Properties.Resources.hangman5,
+                    Properties.Resources.hangman6
+                };
+            }
+            catch (Exception ex)
+            {
+                // Resimler yüklenirken bir hata olursa kullanıcıya gösteriliyor
+                MessageBox.Show("Resimler yüklenirken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        // Yeni bir oyun başlatma fonksiyonu
+        private void oyunbasla()
+        {
+            // Rastgele bir şehir seçiyoruz
+            Random random = new Random();
+            secilensehir = sehirler[random.Next(sehirler.Count)].ToUpper();
+
+            // Seçilen şehrin her harfini "_" olarak maskeliyoruz
+            maskesehir = new string('_', secilensehir.Length);
+
+            // Kalan tahmin hakkı 6 olarak başlıyor
+            kalanhak = 6;
+
+            // Tahmin edilen harfleri tutacak set
+            guessedLetters = new HashSet<char>();
+
+            // İlk resim olan adam asmaca başlatılıyor
+            pictureBox1.Image = hangmanImages[0];
+
+            // Ekran üzerindeki bilgileri güncelliyoruz
+            güncelle();
+        }
+
+        // Ekrandaki bilgileri güncelleyen fonksiyon
+        private void güncelle()
+        {
+            // gizli olan şehri ve kalan hakları gösteriyoruz
+            maskelisehir.Text = string.Join(" ", maskesehir.ToCharArray());
+            kalanhakform.Text = $"Kalan Hak: {kalanhak}";  // String  kullanılarak doğru yazım
+            mesaj.Text = "";
+            tahmintxt.Clear();   // Tahmin giriş alanını temizliyoruz
+            tahmintxt.Focus();   // Kullanıcıdan yeni tahmin bekliyoruz
+
+            // Adam asmaca resmini kalan hakka göre değiştiriyoruz
+            pictureBox1.Image = hangmanImages[6 - kalanhak];
+
+            // Eğer tahmin hakkı kalmadıysa oyun bitmiş demektir
+            if (kalanhak == 0)
+            {
+                mesaj.Text = $"Kaybettiniz! Doğru şehir: {secilensehir}";  // String ile doğru şehir gösteriliyor
+                pictureBox1.Image = hangmanImages[6]; // Tam adam asmaca resmi gösteriliyor
+                tahminbutonu.Enabled = false;         // Tahmin butonunu devre dışı bırakıyoruz
+            }
+
+            // Tahmin hakkı varsa buton aktif kalıyor
+            tahminbutonu.Enabled = kalanhak > 0;
+        }
+
+        // Kullanıcının harf tahminini işleyen fonksiyon
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // Kullanıcı bir harf girmemişse uyarı veriyoruz
+            if (tahmintxt.Text.Length == 0)
+            {
+                mesaj.Text = "Lütfen bir harf giriniz!";
+                return;
+            }
+
+            // Kullanıcının girdiği harfi büyük harfe çeviriyoruz
+            char guessedChar = tahmintxt.Text.ToUpper()[0];
+
+            // Girdiği karakter bir harf değilse uyarı veriyoruz
+            if (!char.IsLetter(guessedChar))
+            {
+                mesaj.Text = "Lütfen geçerli bir harf giriniz!";
+                return;
+            }
+
+            // Eğer aynı harfi daha önce tahmin ettiyse tekrar uyarı veriyoruz
+            if (guessedLetters.Contains(guessedChar))
+            {
+                mesaj.Text = "Bu harfi zaten tahmin ettiniz!";
+                return;
+            }
+
+            // Harfi tahmin edilen harfler listesine ekliyoruz
+            guessedLetters.Add(guessedChar);
+
+            // Harfin doğru tahmin olup olmadığını kontrol ediyoruz
+            bool tahmin = false;
+
+            // Eğer seçilen şehirde bu harf varsa, gizli olan şehri güncelliyoruz
+            if (secilensehir.Contains(guessedChar))
+            {
+                char[] maskedArray = maskesehir.ToCharArray();
+                for (int i = 0; i < secilensehir.Length; i++)
+                {
+                    if (secilensehir[i] == guessedChar)
+                    {
+                        maskedArray[i] = guessedChar;
+                        tahmin = true;
+                    }
+                }
+                maskesehir = new string(maskedArray);
+                mesaj.Text = "Doğru tahmin!";
+            }
+            else
+            {
+                // Yanlış tahmin durumunda kalan tahmin hakkını azaltıyoruz
+                kalanhak--;
+                mesaj.Text = "Yanlış tahmin!";
+            }
+
+            // Eğer tüm harfler doğru tahmin edilirse oyunu kazandınız mesajı gösteriyoruz
+            if (maskesehir.Equals(secilensehir))
+            {
+                mesaj.Text = $"Tebrikler! Şehri buldunuz: {secilensehir}";
+                tahminbutonu.Enabled = false;  // Buton devre dışı bırakılıyor
+                return;
+            }
+
+            // Eğer tahmin hakkı kalmadıysa oyunu kaybettiniz mesajı gösteriyoruz
+            if (kalanhak <= 0)
+            {
+                mesaj.Text = $"Kaybettiniz! Doğru şehir: {secilensehir}";
+                tahminbutonu.Enabled = false;  // Buton devre dışı bırakılıyor
+                pictureBox1.Image = hangmanImages[6];  // Tam adam asmaca resmi gösteriliyor
+            }
+
+            // Ekranı tekrar güncelliyoruz
+            güncelle();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Bu metod gerekmedikçe boş bırakılabilir.
+        }
+    }
+}
