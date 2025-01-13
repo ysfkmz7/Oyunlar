a7483a07f02763859ae7c376e4cb8de5e786d743
Yusuf Kaymaz <146130623+ysfkmz7@users.noreply.github.com>
2025-01-13 13:54:30 +0300

    Add files via upload

---
+++ birds/birds/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace birds
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.highScoreLabel = new System.Windows.Forms.Label();
+            this.birdPictureBox = new System.Windows.Forms.PictureBox();
+            this.pipeTopPictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pipeBottomPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.birdPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTopPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottomPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(243, 49);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(0, 20);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Click += new System.EventHandler(this.scoreLabel_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            // 
+            // highScoreLabel
+            // 
+            this.highScoreLabel.AutoSize = true;
+            this.highScoreLabel.Location = new System.Drawing.Point(26, 27);
+            this.highScoreLabel.Name = "highScoreLabel";
+            this.highScoreLabel.Size = new System.Drawing.Size(51, 20);
+            this.highScoreLabel.TabIndex = 4;
+            this.highScoreLabel.Text = "label1";
+            // 
+            // birdPictureBox
+            // 
+            this.birdPictureBox.Image = global::birds.Properties.Resources.bird;
+            this.birdPictureBox.Location = new System.Drawing.Point(49, 185);
+            this.birdPictureBox.Name = "birdPictureBox";
+            this.birdPictureBox.Size = new System.Drawing.Size(28, 28);
+            this.birdPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.birdPictureBox.TabIndex = 2;
+            this.birdPictureBox.TabStop = false;
+            // 
+            // pipeTopPictureBox
+            // 
+            this.pipeTopPictureBox.Image = global::birds.Properties.Resources.pipedown;
+            this.pipeTopPictureBox.Location = new System.Drawing.Point(526, -2);
+            this.pipeTopPictureBox.Name = "pipeTopPictureBox";
+            this.pipeTopPictureBox.Size = new System.Drawing.Size(58, 127);
+            this.pipeTopPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTopPictureBox.TabIndex = 0;
+            this.pipeTopPictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::birds.Properties.Resources.ground;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 450);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(840, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pipeBottomPictureBox
+            // 
+            this.pipeBottomPictureBox.Image = global::birds.Properties.Resources.pipe;
+            this.pipeBottomPictureBox.Location = new System.Drawing.Point(526, 355);
+            this.pipeBottomPictureBox.Name = "pipeBottomPictureBox";
+            this.pipeBottomPictureBox.Size = new System.Drawing.Size(58, 127);
+            this.pipeBottomPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottomPictureBox.TabIndex = 6;
+            this.pipeBottomPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(843, 478);
+            this.Controls.Add(this.pipeBottomPictureBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.highScoreLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.birdPictureBox);
+            this.Controls.Add(this.pipeTopPictureBox);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.birdPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTopPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottomPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pipeTopPictureBox;
+        private System.Windows.Forms.PictureBox birdPictureBox;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label highScoreLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pipeBottomPictureBox;
+    }
+}
+
---
+++ birds/birds/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace birds
+{
+    public partial class Form1 : Form
+    {
+        // Oyun değişkenleri
+        int pipeSpeed = 8;        // Boruların hareket hızı
+        int gravity = 15;         // Kuşun düşme hızı
+        int score = 0;            // Skor
+        int highScore = 0;        // En yüksek skor
+        int level = 1;            // Oyun seviyesi
+        bool isGameOver = false;  // Oyun bitiş durumu
+        bool isGameStarted = false; // Oyun durumu
+        Random randomGenerator; // Rastgele sayı üretici
+
+        public Form1()
+        {
+            InitializeComponent();
+            randomGenerator = new Random(); // Rastgele sayı üreticiyi başlat
+
+            // Timer'ı ayarla ve başlat
+            gameTimer.Interval = 20; // Her 20ms'de bir çalıştır
+            gameTimer.Tick += new EventHandler(gameTimer_Tick); // Tick olayını bağla
+            gameTimer.Start(); // Timer'ı başlat
+
+            // Klavye olaylarını forma bağla
+            this.KeyDown += new KeyEventHandler(gameKeyDown);
+            this.KeyUp += new KeyEventHandler(gameKeyUp);
+
+            // Başlangıçta boruları rastgele ayarla
+            SetRandomPipePositions();
+
+            // En yüksek skoru göster
+            highScoreLabel.Text = "En Yüksek Skor: " + highScore;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // Oyun bitmediyse devam et
+            if (!isGameOver)
+            {
+                if (isGameStarted) // Oyun başladıysa
+                {
+                    // Kuşun düşme hareketi
+                    birdPictureBox.Top += gravity;
+
+                    // Boruların sola hareketi
+                    pipeTopPictureBox.Left -= pipeSpeed;
+                    pipeBottomPictureBox.Left -= pipeSpeed;
+
+                    // Skor güncelleme
+                    scoreLabel.Text = "Score: " + score;
+
+                    // Boruların geri sarılması
+                    if (pipeTopPictureBox.Left < -pipeTopPictureBox.Width)
+                    {
+                        SetRandomPipePositions(); // Boruları rastgele ayarla
+                        pipeTopPictureBox.Left = ClientSize.Width;
+                        pipeBottomPictureBox.Left = ClientSize.Width;
+                        score++;
+
+                        // Her 1s skorda bir hızı artır
+                        if (score % 1 == 0) // Her 1 puanda hızı artır
+                        {
+                            pipeSpeed++; // Boru hızını artır
+                        }
+                    }
+
+                    // Kuşun borulara veya zemine çarpma kontrolü
+                    if (birdPictureBox.Bounds.IntersectsWith(pipeTopPictureBox.Bounds) ||
+                        birdPictureBox.Bounds.IntersectsWith(pipeBottomPictureBox.Bounds) ||
+                        birdPictureBox.Top < 0 || birdPictureBox.Bottom > ClientSize.Height)
+                    {
+                        EndGame();
+                    }
+                }
+            }
+        }
+
+        private void gameKeyDown(object sender, KeyEventArgs e)
+        {
+            // Space tuşuna basıldığında kuşu yukarı hareket ettir
+            if (e.KeyCode == Keys.Space)
+            {
+                if (!isGameStarted) // Oyun başlamadıysa
+                {
+                    isGameStarted = true; // Oyunu başlat
+                }
+                if (isGameOver)
+                {
+                    // Oyun bittiğinde yeniden başlat
+                    ResetGame();
+                }
+                else
+                {
+                    gravity = -17; // Zıplama kuvveti
+                }
+            }
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e)
+        {
+            // Space tuşu bırakıldığında yerçekimi normale döner
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5; // Normal yerçekimi
+            }
+        }
+
+        private void EndGame()
+        {
+            isGameOver = true; // Oyun bitiyor
+            gameTimer.Stop(); // Timer'ı durdur
+
+            // Oyun bittiğinde en yüksek skoru güncelle
+            if (score > highScore)
+            {
+                highScore = score;
+                highScoreLabel.Text = "En Yüksek Skor: " + highScore; // En yüksek skoru güncelle
+            }
+
+            // Oyun bittiğinde mesaj kutusu göster
+            MessageBox.Show("Kaybettiniz! Puanınız: " + score, "Oyun Bitti", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ResetGame()
+        {
+            // Oyun değişkenlerini sıfırla
+            isGameOver = false;
+            isGameStarted = false; // Oyun sıfırlandığında başlangıç durumu
+            gravity = 15; // Başlangıç yerçekimi
+            score = 0; // Skoru sıfırla
+            level = 1; // Seviyeyi sıfırla
+            pipeSpeed = 8; // Boru hızını başlangıç değerine döndür
+
+            // Boruların başlangıç konumunu ayarla
+            pipeTopPictureBox.Left = ClientSize.Width;
+            pipeBottomPictureBox.Left = ClientSize.Width;
+
+            // Kuşun başlangıç konumunu ayarla
+            birdPictureBox.Top = ClientSize.Height / 2;
+
+            // Timer'ı başlat
+            gameTimer.Start();
+        }
+
+        private void SetRandomPipePositions()
+        {
+            // Borular için rastgele yükseklik ayarla
+            int pipeHeightTop = randomGenerator.Next(100, 300); // Üst borunun yüksekliğini rastgele seç
+            pipeTopPictureBox.Height = pipeHeightTop; // Üst borunun yüksekliğini ayarla
+
+            int pipeHeightBottom = ClientSize.Height - pipeHeightTop - 150; // Alt borunun yüksekliğini hesapla (150 piksel boşluk bırak)
+            pipeBottomPictureBox.Height = pipeHeightBottom; // Alt borunun yüksekliğini ayarla
+
+            // Alt borunun üst kenarını ayarla
+            pipeBottomPictureBox.Top = pipeHeightTop + 150; // Alt borunun konumunu ayarla
+        }
+
+        private void scoreLabel_Click(object sender, EventArgs e)
+        {
+            // Bu event'i kullanmıyorsan boş bırakabilirsin
+        }
+    }
+}
